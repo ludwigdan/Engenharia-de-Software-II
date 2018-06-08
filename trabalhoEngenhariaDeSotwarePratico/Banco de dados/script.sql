bb227b74582a623646b4e6c29e5cf8537765f828
Danrlei <141850@upf.br>
2018-06-07 21:27:08 -0300

    Novas tabelas

--- trabalhoEngenhariaDeSotwarePratico/Banco de dados/script.sql
+++ trabalhoEngenhariaDeSotwarePratico/Banco de dados/script.sql
@@ -14,3 +14,26 @@ insert into ingrediente values (nextval('seqIdIngrediente'), 'Hamburguer de Cost
 
 
 alter table ingrediente add constraint Ck1_ingrediente CHECK (unidadeMedida IN ('Unidade', 'MG', 'ML'));
+
+create table ingrediente(
+	idIngrediente integer primary key,
+	descricao varchar(100) not null,
+	unidadeMedida varchar(10) not null,
+	porcao numeric(10,2) not null,
+	preco numeric(10,2) not null
+);
+
+create table itemCardapio(
+	id  integer primary key,
+	percentualLucro numeric(10,2) not null,
+	precoVendaIndicado numeric(10,2) not null,
+	descricao varchar(100) not null
+);
+
+create sequence seqIdItemCardapio;
+
+create table ingredienteItemCardapio(
+	idIngrediente integer,
+	idItem integer,
+	quantidade integer
+);
