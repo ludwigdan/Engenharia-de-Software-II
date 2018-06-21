9da80ff91d7335f4e4cc00de1172c9fb1ec402e0
Danrlei <141850@upf.br>
2018-06-21 20:48:51 -0300

    Removendo arquivo desnecessários

--- trabalhoEngenhariaDeSotwarePratico/exemplos/aulaPHP/crudPHP/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-    $dsn = "mysql:host=localhost;dbname=ads";
-    $usuario = "root";
-    $senha = "root";
-
-    $bd = new PDO($dsn, $usuario, $senha);
-?>
--- trabalhoEngenhariaDeSotwarePratico/exemplos/aulaPHP/estudo_caso/crud_aluno_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    
-    if(!isset($_GET['acao'])) $acao = "listar";
-    else $acao = $_GET['acao'];
-
-    if($acao == "listar"){
-        $sql = "SELECT * FROM aluno";
-    }
